1b1eef0b5b1c44962affe0edf2149f81ce11734c
Vadim Kantorov <vadimkantorov@gmail.com>
2020-10-26 23:04:32 +0000

    ..

--- models.py
+++ models.py
@@ -55,6 +55,7 @@ class SincConv1d(nn.Conv1d):
 		**kwargs,
 		):	
 		super().__init__(in_channels, out_channels, kernel_size, **kwargs)
+		self.register_parameter('weight', None)
 
 		to_mel = lambda hz: 2595 * math.log10(1 + hz / 700)
 		to_hz = lambda mel: 700 * (10 ** (mel / 2595) - 1)
@@ -69,8 +70,8 @@ class SincConv1d(nn.Conv1d):
 
 		hz = to_hz(torch.linspace(to_mel(low_hz), to_mel(high_hz), out_channels + 1))
 
-		self.low_hz_ = nn.Parameter(torch.Tensor(hz[:-1]).view(-1, 1))
-		self.band_hz_ = nn.Parameter(torch.Tensor(ediff1d(hz)).view(-1, 1))
+		self.low_hz_ = nn.Parameter(hz[:-1].view(-1, 1))
+		self.band_hz_ = nn.Parameter(ediff1d(hz).view(-1, 1))
 
 		# Half Hamming half window
 		n_lin = torch.linspace(0, kernel_size / 2 - 1, steps=int((kernel_size / 2)))
@@ -80,9 +81,9 @@ class SincConv1d(nn.Conv1d):
 		self.n_ = 2 * math.pi * torch.arange(-n, 0).view(1, -1) / sample_rate
 
 	@property
-	def weight_computed(self):
+	def weight(self):
 		low = self.min_low_hz + torch.abs(self.low_hz_)
-		high = clamp(low + self.min_band_hz + torch.abs(self.band_hz_)).clamp(min = self.min_low_hz, max = self.sample_rate / 2)
+		high = (low + self.min_band_hz + torch.abs(self.band_hz_)).clamp(min = self.min_low_hz, max = self.sample_rate / 2)
 		band = (high - low)[:, 0]
 
 		f_times_t_low = torch.matmul(low, self.n_)
@@ -94,18 +95,14 @@ class SincConv1d(nn.Conv1d):
 
 		band_pass = torch.cat([band_pass_left, band_pass_center, band_pass_right], dim=1)
 		band_pass = band_pass / (2 * band[:, None])
-		return band_pass.view_as(self.weight)
-
-	def forward(self, x):
-		self.weight.copy_(self.weight_computed)
-		return super().forward(x)
+		return band_pass.view(self.out_channels, 1, -1)
 		
 
 class SincNet(nn.Module):
 	def __init__(
 		self,
 		waveform_normalize=True,
-		sample_rate=16000,
+		sample_rate=16_000,
 		min_low_hz=50,
 		min_band_hz=50,
 		out_channels=[80, 60, 60],
@@ -135,7 +132,7 @@ class SincNet(nn.Module):
 			in_channels = out_channels
 
 	def forward(self, waveforms):
-		output = waveforms.transpose(1, 2)
+		output = waveforms.unsqueeze(1)
 		output = self.waveform_normalize_(output)
 		for i, (conv1d, max_pool1d) in enumerate(zip(self.conv1d_, self.max_pool1d_)):
 			output = conv1d(output)
@@ -144,15 +141,14 @@ class SincNet(nn.Module):
 			output = self.instance_norm1d_[i](output)
 			output = self.activation_(output)
 			output = self.dropout_(output)
-
-		return output.transpose(1, 2)
+		return output
 
 class TDNN(nn.Module):
 	def __init__(
 		self,
+		in_channels: int,
+		out_channels: int,
 		context: list,
-		input_channels: int,
-		output_channels: int,
 		full_context: bool = True,
 	):
 		super(TDNN, self).__init__()
@@ -165,45 +161,27 @@ class TDNN(nn.Module):
 			dilation = context[1] - context[0]
 			kernel_size = len(context)
 		
-		self.temporal_conv = torch.nn.utils.weight_norm(nn.Conv1d(input_channels, output_channels, kernel_size, dilation = dilation))
+		self.temporal_conv = torch.nn.utils.weight_norm(nn.Conv1d(in_channels, out_channels, kernel_size, dilation = dilation))
 
 	def forward(self, x):
-		x = self.temporal_conv(torch.transpose(x, 1, 2))
-		return F.relu(torch.transpose(x, 1, 2))
+		return F.relu(self.temporal_conv(x))
 
 class StatsPool(nn.Module):
 	def forward(self, x: torch.Tensor) -> torch.Tensor:
-		mean, std = torch.mean(x, dim=1), torch.std(x, dim=1)
+		std, mean = torch.std_mean(x, dim = -1)
 		return torch.cat((mean, std), dim=1)
 
 class XVectorNet(nn.Module):
 	def __init__(self, input_dim: int = 24, embedding_dim: int = 512):
 		super(XVectorNet, self).__init__()
-		frame1 = TDNN(
-			context=[-2, 2],
-			input_channels=input_dim,
-			output_channels=512,
-			full_context=True,
-		)
-
-		frame2 = TDNN(
-			context=[-2, 0, 2],
-			input_channels=512,
-			output_channels=512,
-			full_context=False,
-		)
-		frame3 = TDNN(
-			context=[-3, 0, 3],
-			input_channels=512,
-			output_channels=512,
-			full_context=False,
-		)
-		frame4 = TDNN(context=[0], input_channels=512, output_channels=512, full_context=True)
-		frame5 = TDNN(context=[0], input_channels=512, output_channels=1500, full_context=True)
+		frame1 = TDNN(in_channels=input_dim, out_channels=512, context=[-2, 2   ], full_context=True)
+		frame2 = TDNN(in_channels=      512, out_channels=512, context=[-2, 0, 2], full_context=False)
+		frame3 = TDNN(in_channels=      512, out_channels=512, context=[-3, 0, 3], full_context=False)
+		frame4 = TDNN(in_channels=      512, out_channels=512, context=[0       ], full_context=True)
+		frame5 = TDNN(in_channels=      512, out_channels=1500,context=[0       ], full_context=True)
 		self.tdnn = nn.Sequential(frame1, frame2, frame3, frame4, frame5, StatsPool())
-		self.segment6 = nn.Linear(3000, embedding_dim)
+		self.segment6 = nn.Linear(2*1500, embedding_dim)
 		self.segment7 = nn.Linear(embedding_dim, embedding_dim)
-		self.embedding_dim = embedding_dim
 	
 	def forward(self, x: torch.Tensor, return_intermediate: typing.Optional[str] = None):
 		x = self.tdnn(x)
--- train.py
+++ train.py
@@ -9,5 +9,15 @@ tdnn = dict(embedding_dim = 512)
 embedding = dict(batch_normalize = False)
 
 model = models.SincTDNN(sincnet = sincnet, tdnn = tdnn, embedding = embedding)
-model.load_state_dict(weights, strict = False)
+model.load_state_dict(weights)
 print(model)
+
+waveforms = (torch.rand(16, 4*1024) - 0.5) * 2
+print('Before forward')
+features = model(waveforms)
+print('Before backward')
+
+print(waveforms.shape, features.shape)
+
+features.backward(torch.ones_like(features))
+print('After backward')
