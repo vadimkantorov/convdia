52a01810ab4f3e30c33646afe06f50acf3148d98
Vadim Kantorov <vadimkantorov@gmail.com>
2020-11-05 14:01:51 +0000

    ...

--- models.py
+++ models.py
@@ -7,186 +7,167 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class Scaling(nn.Module):
-	def __init__(self, n_features : int, method : typing.Literal['fixed', 'logistic']):
-		super().__init__()
-		self.method = method
-		self.linear = nn.Linear(1, 1, bias = True) if self.method == 'logistic' else nn.Identity()
-		self.activation = nn.Sigmoid() if self.method == 'logistic' else nn.Identity()
-
-	def forward(self, features : torch.Tensor):
-		if self.method is None:
-			return features
-		norm = features.norm(p = 2, dim = -1, keepdim = True)
-		new_norm = self.activation(self.linear(norm)) if self.method == 'logistic' else 1.0
-		return new_norm / (norm + 1e-6) * features
-
-class Embedding(nn.Module):
-	def __init__(self, n_features : int, batch_normalize : bool = False, scale : str = None):
-		super().__init__()
-		self.batch_normalize_ = nn.BatchNorm1d(n_features, eps = 1e-5, momentum = 0.1, affine = False) if batch_normalize else nn.Identity()
-		self.scaling = Scaling(n_features, method = scale)
-
-	def forward(self, embedding : torch.Tensor):
-		return self.scaling(self.batch_normalize_(embedding))
-
 class SincTDNN(nn.Module):
-	def __init__(self, sincnet: typing.Optional[dict] = None, tdnn: typing.Optional[dict] = None, embedding: typing.Optional[dict] = None):
+	def __init__(self, sincnet: dict = {}, tdnn: dict = {}):
 		super().__init__()
 		self.sincnet_ = SincNet(**sincnet)
-		n_features = self.sincnet_.out_channels[-1]
-		self.tdnn_ = XVectorNet(n_features, **tdnn)
-		self.embedding_ = Embedding(n_features, **embedding)
+		self.tdnn_ = XVectorNet(self.sincnet_.out_channels[-1], **tdnn)
 
-	def forward(self, waveforms : torch.Tensor):
-		output = self.sincnet_(waveforms)
-		output = self.tdnn_(output)
-		return self.embedding_(output)
+	def forward(self, waveforms : torch.Tensor, eps : float = 1e-6):
+		x = self.sincnet_(waveforms)
+		x = self.tdnn_(x)
+		return x / (x.norm(p = 2, dim = -1, keepdim = True) + eps) 
 
-class SincConv1d(nn.Conv1d):
-	def __init__(
-		self,
-		in_channels,
-		out_channels,
-		kernel_size,
-		sample_rate=16000,
-		min_low_hz=50,
-		min_band_hz=50,
-		**kwargs,
-		):	
-		super().__init__(in_channels, out_channels, kernel_size, **kwargs)
+class SincConv1d(torch.nn.Conv1d):
+	def __init__(self, in_channels, out_channels, kernel_size, stride = 1, padding = 0, dilation = 1, groups = 1, bias = False, padding_mode = 'zeros', sample_rate = 16_000, min_low_hz = 50, min_band_hz = 50, low_hz = 30):
+		assert in_channels == 1 and kernel_size % 2 == 1 and bias is False and groups == 1
+		super().__init__(in_channels, out_channels, kernel_size, stride = stride, padding = padding, dilation = dilation, groups = groups, bias = bias, padding_mode = padding_mode)
 		self.register_parameter('weight', None)
 
-		to_mel = lambda hz: 2595 * math.log10(1 + hz / 700)
+		to_mel = lambda hz: 2595 * torch.log10(1 + hz / 700)
 		to_hz = lambda mel: 700 * (10 ** (mel / 2595) - 1)
-		ediff1d = lambda t: t[1:] - t[:-1]
+		ediff1d = lambda t, prepend = False: t[1:] - t[:-1]
 
 		self.sample_rate = sample_rate
 		self.min_low_hz = min_low_hz
 		self.min_band_hz = min_band_hz
 
-		low_hz = 30
 		high_hz = sample_rate / 2 - (min_low_hz + min_band_hz)
 
-		hz = to_hz(torch.linspace(to_mel(low_hz), to_mel(high_hz), out_channels + 1))
+		hz = to_hz(torch.linspace(to_mel(torch.tensor(float(low_hz))), to_mel(torch.tensor(float(high_hz))), steps = out_channels + 1))
 
-		self.low_hz_ = nn.Parameter(hz[:-1].view(-1, 1))
-		self.band_hz_ = nn.Parameter(ediff1d(hz).view(-1, 1))
+		self.low_hz_ = nn.Parameter(hz[:-1].unsqueeze(-1))
+		self.band_hz_ = nn.Parameter(ediff1d(hz).unsqueeze(-1))
 
-		# Half Hamming half window
-		n_lin = torch.linspace(0, kernel_size / 2 - 1, steps=int((kernel_size / 2)))
-		self.window_ = 0.54 - 0.46 * torch.cos(2 * math.pi * n_lin / kernel_size)
+		self.window = 0.54 - 0.46 * torch.cos(2 * math.pi * torch.linspace(0, kernel_size / 2 - 1, steps=int((kernel_size / 2))) / kernel_size)
+		#self.window = torch.hamming_window(kernel_size)[:kernel_size // 2]
 		
-		n = (kernel_size - 1) / 2.0
-		self.n_ = 2 * math.pi * torch.arange(-n, 0).view(1, -1) / sample_rate
+		self.sinct = 2 * math.pi * torch.arange(-(kernel_size // 2), 0, dtype = torch.float32) / sample_rate
 
 	@property
 	def weight(self):
-		low = self.min_low_hz + torch.abs(self.low_hz_)
-		high = (low + self.min_band_hz + torch.abs(self.band_hz_)).clamp(min = self.min_low_hz, max = self.sample_rate / 2)
-		band = (high - low)[:, 0]
+		if self._buffers.get('weight') is not None:
+			return self._buffers['weight']
 
-		f_times_t_low = torch.matmul(low, self.n_)
-		f_times_t_high = torch.matmul(high, self.n_)
+		lo = self.min_low_hz + self.low_hz_.abs()
+		hi = (lo + self.min_band_hz + self.band_hz_.abs()).clamp(min = self.min_low_hz, max = self.sample_rate / 2)
+		sincarg_hi, sincarg_lo = hi * self.sinct, lo * self.sinct
 
-		band_pass_left = (f_times_t_high.sin() - f_times_t_low.sin()) / (self.n_ / 2) * self.window_
-		band_pass_center = 2 * band.view(-1, 1)
-		band_pass_right = band_pass_left.flip(dims=[1])
-
-		band_pass = torch.cat([band_pass_left, band_pass_center, band_pass_right], dim=1)
-		band_pass = band_pass / (2 * band[:, None])
-		return band_pass.view(self.out_channels, 1, -1)
+		band_pass_left = (sincarg_hi.sin() - sincarg_lo.sin()) / (self.sinct / 2) * self.window
+		band_pass_center = (hi - lo) * 2
+		band_pass_right = band_pass_left.flip(dims = [1])
+		band_pass = torch.cat([band_pass_left, band_pass_center, band_pass_right], dim = 1) / band_pass_center
+		
+		return band_pass.unsqueeze(1)
 		
 
 class SincNet(nn.Module):
 	def __init__(
 		self,
-		waveform_normalize=True,
-		sample_rate=16_000,
-		min_low_hz=50,
-		min_band_hz=50,
-		out_channels=[80, 60, 60],
-		kernel_size: typing.List[int] = [251, 5, 5],
-		stride=[1, 1, 1],
-		max_pool=[3, 3, 3],
-		instance_normalize=True,
-		activation="leaky_relu",
-		dropout=0.0,
+		waveform_normalize = True,
+		sample_rate = 16_000,
+		min_low_hz = 50,
+		min_band_hz = 50,
+		out_channels = [80, 60, 60],
+		kernel_size = [251, 5, 5],
+		stride = [5, 1, 1],
+		max_pool = [3, 3, 3],
+		instance_normalize = True,
+		activation = 'leaky_relu',
+		dropout = 0.0,
+		padding_same = True
 	):
 		super().__init__()
 		self.out_channels = out_channels
-		self.waveform_normalize_ = torch.nn.InstanceNorm1d(1, affine = True) if waveform_normalize else nn.Identity()
+		self.waveform_normalize_ = nn.InstanceNorm1d(1, affine = True) if waveform_normalize else nn.Identity()
 		self.activation_ = nn.LeakyReLU(negative_slope = 0.2) if activation == 'leaky_relu' else nn.Identity()
 		self.dropout_ = nn.Dropout(p = dropout) if dropout else nn.Identity()
 		self.conv1d_ = nn.ModuleList()
 		self.max_pool1d_ = nn.ModuleList()
 		self.instance_norm1d_ = nn.ModuleList()
 
-		config = zip(out_channels, kernel_size, stride, max_pool)
 		in_channels = 1
-		for i, (out_channels, kernel_size, stride, max_pool) in enumerate(config):
-			conv_kwargs = dict(in_channels = in_channels, out_channels = out_channels, kernel_size = kernel_size, stride = stride, padding = 0, dilation = 1, groups = 1)
+		for i, (out_channels, kernel_size, stride, max_pool) in enumerate(zip(out_channels, kernel_size, stride, max_pool)):
+			conv_kwargs = dict(in_channels = in_channels, out_channels = out_channels, kernel_size = kernel_size, stride = stride, padding = kernel_size // 2 if padding_same else 0)
 			self.conv1d_.append(nn.Conv1d(**conv_kwargs, bias = True) if i > 0 else SincConv1d(**conv_kwargs, bias = False, sample_rate = sample_rate, min_low_hz = min_low_hz, min_band_hz = min_band_hz))
-			self.max_pool1d_.append(nn.MaxPool1d(max_pool, stride=max_pool, padding=0, dilation=1))
-			self.instance_norm1d_.append(nn.InstanceNorm1d(out_channels, affine=True) if instance_normalize else nn.Identity())
+			self.max_pool1d_.append(nn.MaxPool1d(max_pool, stride = max_pool, padding = max_pool // 2 if padding_same else 0))
+			self.instance_norm1d_.append(nn.InstanceNorm1d(out_channels, affine = True) if instance_normalize else nn.Identity())
 			in_channels = out_channels
 
 	def forward(self, waveforms):
-		output = waveforms.unsqueeze(1)
-		output = self.waveform_normalize_(output)
+		x = waveforms.unsqueeze(1)
+		x = self.waveform_normalize_(x)
 		for i, (conv1d, max_pool1d) in enumerate(zip(self.conv1d_, self.max_pool1d_)):
-			output = conv1d(output)
-			output = torch.abs(output) if i == 0 else output
-			output = max_pool1d(output)
-			output = self.instance_norm1d_[i](output)
-			output = self.activation_(output)
-			output = self.dropout_(output)
-		return output
+			x = conv1d(x)
+			x = x.abs() if i == 0 else x
+			x = max_pool1d(x)
+			x = self.instance_norm1d_[i](x)
+			x = self.activation_(x)
+			x = self.dropout_(x)
+		return x
 
-class TDNN(nn.Module):
-	def __init__(
-		self,
-		in_channels: int,
-		out_channels: int,
-		context: list,
-		full_context: bool = True,
-	):
-		super(TDNN, self).__init__()
-		context = sorted(context)
+class WeightNormConv1d(nn.Conv1d):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.weight_v = nn.Parameter(self.weight.detach())
+		self.weight_g = nn.Parameter(self.weight.norm(dim = [1, 2], keepdim = True).detach())
+		self.register_parameter('weight', None)
 
-		if full_context:
-			kernel_size = context[-1] - context[0] + 1 if len(context) > 1 else 1
-			dilation = 1
-		else:
-			dilation = context[1] - context[0]
-			kernel_size = len(context)
-		
-		self.temporal_conv = torch.nn.utils.weight_norm(nn.Conv1d(in_channels, out_channels, kernel_size, dilation = dilation))
+	@property
+	def weight(self):
+		return self.weight_v * self.weight_g / self.weight_v.norm(dim = [1, 2], keepdim = True)
+
+class WeightNormConv1dReLU(nn.Module):
+	def __init__(self, *args, **kwargs):
+		super(WeightNormConv1dReLU, self).__init__()
+		self.temporal_conv = WeightNormConv1d(*args, **kwargs)
 
 	def forward(self, x):
-		return F.relu(self.temporal_conv(x))
+		x = self.temporal_conv(x)
+		x = F.relu(x)
+		return x
 
-class StatsPool(nn.Module):
-	def forward(self, x: torch.Tensor) -> torch.Tensor:
-		std, mean = torch.std_mean(x, dim = -1)
-		return torch.cat((mean, std), dim=1)
+class MeanStdPool1d(nn.Module):
+	def __init__(self, kernel_size = None, stride = None):
+		super().__init__()
+		self.kernel_size = kernel_size
+		self.stride = stride
+
+	def forward(self, x):
+		kwargs = dict(kernel_size = self.kernel_size, padding = self.kernel_size // 2, stride = self.stride or 1) if self.kernel_size is not None else {}
+		mean = F.avg_pool1d(x, **kwargs) if kwargs else F.adaptive_avg_pool1d(x, output_size = 1)
+		mean = F.interpolate(mean, x.shape[-1]) if kwargs and kwargs['stride'] != 1 else mean
+
+		zero_mean_squared = (x - mean) ** 2
+		std_squared = F.avg_pool1d(zero_mean_squared, **kwargs) if kwargs else F.adaptive_avg_pool1d(zero_mean_squared, output_size = 1) 
+		std = std_squared.sqrt()
+		return torch.cat((mean, std), dim = 1)
 
 class XVectorNet(nn.Module):
-	def __init__(self, input_dim: int = 24, embedding_dim: int = 512):
-		super(XVectorNet, self).__init__()
-		frame1 = TDNN(in_channels=input_dim, out_channels=512, context=[-2, 2   ], full_context=True)
-		frame2 = TDNN(in_channels=      512, out_channels=512, context=[-2, 0, 2], full_context=False)
-		frame3 = TDNN(in_channels=      512, out_channels=512, context=[-3, 0, 3], full_context=False)
-		frame4 = TDNN(in_channels=      512, out_channels=512, context=[0       ], full_context=True)
-		frame5 = TDNN(in_channels=      512, out_channels=1500,context=[0       ], full_context=True)
-		self.tdnn = nn.Sequential(frame1, frame2, frame3, frame4, frame5, StatsPool())
-		self.segment6 = nn.Linear(2*1500, embedding_dim)
+	def __init__(self, input_dim: int = 24, embedding_dim: int = 512, hidden_dim_small: int = 512, hidden_dim_large: int = 1500, padding_same: bool = True, kernel_size = [5, 3, 3, 1, 1], dilation = [1, 2, 3, 1, 1], kernel_size_pool: typing.Optional[int] = None, stride_pool: typing.Optional[int] = None):
+		super().__init__()
+		conv_args = lambda k: dict(kernel_size = kernel_size[k], padding = dilation[k] * (kernel_size[k] // 2) if padding_same else 0, dilation = dilation[k], stride = 1)
+		self.tdnn = nn.Sequential(
+			WeightNormConv1dReLU(input_dim,        hidden_dim_small, **conv_args(0)),
+			WeightNormConv1dReLU(hidden_dim_small, hidden_dim_small, **conv_args(1)),
+			WeightNormConv1dReLU(hidden_dim_small, hidden_dim_small, **conv_args(2)),
+			WeightNormConv1dReLU(hidden_dim_small, hidden_dim_small, **conv_args(3)),
+			WeightNormConv1dReLU(hidden_dim_small, hidden_dim_large, **conv_args(4))
+		)
+		self.pool = MeanStdPool1d(kernel_size = kernel_size_pool, stride = stride_pool)
+		self.segment6 = nn.Linear(hidden_dim_large * 2, embedding_dim)
 		self.segment7 = nn.Linear(embedding_dim, embedding_dim)
 	
-	def forward(self, x: torch.Tensor, return_intermediate: typing.Optional[str] = None):
+	def forward(self, x: torch.Tensor):
 		x = self.tdnn(x)
-		if return_intermediate == 'stats_pool': return x
-		x = self.segment6(x)
-		if return_intermediate == 'segment6': return x
-		x = self.segment7(F.relu(x))
-		return F.relu(x)
+
+		x = self.pool(x)
+		
+		x = F.conv1d(x, self.segment6.weight.unsqueeze(-1))
+		x = F.relu(x)
+		
+		x = F.conv1d(x, self.segment7.weight.unsqueeze(-1))
+		x = F.relu(x)
+		
+		return x
+
--- train.py
+++ train.py
@@ -1,23 +1,49 @@
+import json
+import numpy as np
+import soundfile
 import torch
 import models
 
-weights_path = 'emb_voxceleb/train/X.SpeakerDiarization.VoxCeleb.train/weights/0326.pt'
-weights = torch.load(weights_path)
+def read_audio(audio_path, mono = False, dtype = 'float32'):
+	smax = torch.iinfo(torch.int16).max
+	f2s_numpy = lambda signal, max = np.float32(smax): np.multiply(signal, max).astype('int16')
+	s2f_numpy = lambda signal, max = np.float32(smax): np.divide(signal, max, dtype = 'float32')
+	
+	signal, sample_rate_ = soundfile.read(audio_path, dtype = 'int16')
+	signal = signal[:, None] if len(signal.shape) == 1 else signal
+	signal = signal.T
+	
+	if signal.dtype == np.int16 and dtype == 'float32':
+		signal = s2f_numpy(signal)
+	
+	if mono and len(signal) > 1:
+		assert signal.dtype == np.float32
+		signal = signal.mean(0, keepdims = True)
 
-sincnet = dict(stride = [5, 1, 1], waveform_normalize = True, instance_normalize = True)
-tdnn = dict(embedding_dim = 512)
-embedding = dict(batch_normalize = False)
+	signal = torch.as_tensor(signal)
 
-model = models.SincTDNN(sincnet = sincnet, tdnn = tdnn, embedding = embedding)
-model.load_state_dict(weights)
-print(model)
+	return signal, sample_rate_
 
-waveforms = (torch.rand(16, 4*1024) - 0.5) * 2
-print('Before forward')
-features = model(waveforms)
-print('Before backward')
+if __name__ == '__main__':
+	transcript_path = 'subset.json'
+	weights_path = 'emb_voxceleb/train/X.SpeakerDiarization.VoxCeleb.train/weights/0326.pt'
 
-print(waveforms.shape, features.shape)
+	torch.set_grad_enabled(False)
+	model = models.SincTDNN(tdnn = dict(kernel_size_pool = 21, stride_pool = 3))
+	
+	model.load_state_dict(torch.load(weights_path))
+	model.eval()
 
-features.backward(torch.ones_like(features))
-print('After backward')
+	transcript = json.load(open(transcript_path))
+	#embeddings = open('embeddings.tsv', 'w')
+	#speaker_names = open('speaker_names.tsv', 'w')
+	for i, t in enumerate(transcript):
+		channels, sample_rate = read_audio(t['audio_path'], dtype = 'float32', mono = False)  #(torch.rand(16, 4*1024) - 0.5) * 2
+		features = model(channels)
+		break
+		#embeddings.write('\t'.join(map('{:.02f}'.format, features[0].cpu().clone().tolist())) + '\n')
+		#speaker_names.write(t['speaker_name'] + '\n')
+		print(i, '/', len(transcript))
+
+	#print(waveforms.shape, features.shape)
+	#features.backward(torch.ones_like(features))
